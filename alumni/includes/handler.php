057c2ab43693ed495776fd245b9e17a3e82bb48c
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-02-09 11:52:58 +0530

    added 2019 code

---
+++ alumni/includes/handler.php
@@ -0,0 +1,15 @@
+<?php 
+require 'config/config.php';
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	welcome to my new world!
+ 
+ </body>
+ </html>
---
+++ contact/config/config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$timezone = date_default_timezone_set("Europe/London");
+
+$con = mysqli_connect("localhost", "root", "", "tedx"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+?>
